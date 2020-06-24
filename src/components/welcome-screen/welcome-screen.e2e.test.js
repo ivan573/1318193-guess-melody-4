6fb92cee6a626822a011e01538af85ef0476eb8d
Ivan <ivanprivalov@mail.ru>
2020-06-24 14:27:45 +0300

    Optimizing code

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -19,7 +19,7 @@ it(`Should welcome button be pressed`, () => {
 
   const welcomeButton = welcomeScreen.find(`button.welcome__button`);
 
-  welcomeButton.props().onClick();
+  welcomeButton.simulate(`click`);
 
-  expect(onWelcomeButtonClick.mock.calls.length).toBe(1);
+  expect(onWelcomeButtonClick).toHaveBeenCalledTimes(1);
 });
